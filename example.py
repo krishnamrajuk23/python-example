e7c67a01877ba82c3d39f94a8740dc3645d1287f
LAPTOP-JQA1MISB\raju <krishnamrajuk23@gmail.com>
2022-02-14 16:51:23 +0530

    data-types in python

---
+++ example.py
@@ -0,0 +1,16 @@
+print("Hello world");
+my_string = "abcdefghij";
+print(my_string[3:6]);
+print(my_string[::2]);
+# print(2 + '3'); // Error 
+# print(2 - '3') //Error
+
+name = "Jhon";
+age = 30;
+print("My name is {n}".format(n = name));
+print("My name is {n} and age is {a}".format(n=name, a = age));
+print("My name is {} and age is {}".format(name,age));
+
+#version 3.6 format method changed to f
+print(f"My name is {name} and age is {age}")
+
