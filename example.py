6785713d5b107324456e44a3d3f997a4d9da2708
LAPTOP-JQA1MISB\raju <krishnamrajuk23@gmail.com>
2022-03-06 20:52:44 +0530

    resolved errors

--- example.py
+++ example.py
@@ -14,3 +14,20 @@ print("My name is {} and age is {}".format(name,age));
 #version 3.6 format method changed to f
 print(f"My name is {name} and age is {age}")
 
+
+#Dictionaries
+print('-------Dictionaries--------')
+my_dict = {'apple': 2, 'orange': 3}
+print(len(my_dict))
+print(my_dict.keys())
+print(my_dict.values())
+print(my_dict.items())  # items return array of tuples format
+my_dict['apple'] = 10
+print(my_dict['apple'])
+
+
+#Tuples
+print('-------Tuples--------')
+myTuples = (1, 2, 3)
+print(len(myTuples))
+#print(len(myTuples[1]))  # throw error
