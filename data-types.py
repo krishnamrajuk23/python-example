6250bf5d084dacdf30f4331f6fcac69410446b70
LAPTOP-JQA1MISB\raju <krishnamrajuk23@gmail.com>
2022-03-09 00:50:51 +0530

    data type string

--- data-types.py
+++ data-types.py
@@ -8,9 +8,27 @@
 # Sets                    - set - unordered unique objects {"a", "b"}
 # Booleans                - logical values true/false
 
-#Reverse String
+#====== String ===========#
 
-my_string = "abcd"
+my_string = "programming"
+
+# Access first character from string
+print("first character ", my_string[0])
+
+# Access last character from string
+print("last character ", my_string[-1])
+
+# slicing 1st to 5th character
+print("between character ", my_string[1:5])
+
+# slicing 3rd to 2nd last character
+print("between character ", my_string[3:-2])
+
 # start:stop:steps
 reverseStr = my_string[::-1]
-print("my_string value ", reverseStr)
+print("reverse string ", reverseStr)
+
+
+#======= Error =====#
+my_string[5] = 'a'  # error => 'str' object does not support item assignment
+del my_string[1]      # error => 'str' object doesn't support item deletion
