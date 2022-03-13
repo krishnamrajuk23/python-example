149170bbe3f59850882e30df28bda63fee771f15
LAPTOP-JQA1MISB\raju <krishnamrajuk23@gmail.com>
2022-03-13 13:05:02 +0530

    data type file created

--- data-types/list-dt.py
+++ data-types/list-dt.py
@@ -1,4 +1,4 @@
-#-------  List ------#
+#========== List ============#
 
 my_string = "hello"
 myList = []
@@ -6,10 +6,11 @@ for letter in my_string:
     myList.append(letter)
 
 print("Get list elements", myList)
-#short hand of above code
+
+#-------- short hand of above code --------#
+
 elements = [letter for letter in my_string]
 # o/p: same as print statement one
-
 print("short hand code of list assignment", elements)
 
 myList = [x for x in range(0, 10) if x % 2 == 0]
@@ -17,3 +18,6 @@ print("myList values", myList)  # opt: [0,2,4,6,8]
 
 myList = [x**2 for x in range(0, 10) if x % 2 == 0]
 print("myList values", myList)  # opt: [0,4,16,36,64]
+
+
+
